65d38c9e89c0b373500139779a7009a0458b83bc
Anya Brun <tsbrun@icloud.com>
2025-01-30 19:21:50 +0000

    feat: upload, delete, and update cat pics

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,49 +4,79 @@ class PhotosController < ApplicationController
   # GET /photos
   def index
     @photos = Photo.all
-
-    render json: @photos
+    render json: { photos: @photos.map { |photo| format_photo_response(photo) } }
   end
 
   # GET /photos/1
   def show
-    render json: @photo
+    render json: format_photo_response(@photo)
   end
 
   # POST /photos
   def create
     @photo = Photo.new(photo_params)
-    @photo.file.attach(params[:file])
-
+  
     if @photo.save
-      render json: @photo, status: :created, location: @photo
+      attach_file(@photo, file_params[:file]) if file_params.present?
+      render json: format_photo_response(@photo), status: :created
     else
-      render json: @photo.errors, status: :unprocessable_entity
+      render json: { errors: @photo.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /photos/1
   def update
-    if @photo.update(photo_params)
-      render json: @photo
+    updated = false
+
+    if photo_params.present?
+      updated = @photo.update(photo_params)
+    end
+  
+    if file_params.present?
+      attach_file(@photo, file_params[:file])
+      updated = true
+    end
+  
+    if updated
+      render json: format_photo_response(@photo)
     else
-      render json: @photo.errors, status: :unprocessable_entity
+      render json: { errors: @photo.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   # DELETE /photos/1
   def destroy
     @photo.destroy!
+    head :no_content
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_photo
       @photo = Photo.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def photo_params
-      params.require(:photo).permit(:title, :desc)
+      params.permit(:title, :desc)
+    end
+
+    def file_params
+      params.permit(:file)
+    end
+
+    def attach_file(photo, file)
+      return unless file.respond_to?(:path) && file.respond_to?(:content_type)
+    
+      photo.file.purge if photo.file.attached? # Remove old file if updating
+      photo.file.attach(file)
+    end
+
+    # Format JSON response
+    def format_photo_response(photo)
+      {
+        id: photo.id,
+        title: photo.title,
+        desc: photo.desc,
+        file: photo.file.attached? ? url_for(photo.file) : nil
+      }
     end
 end
